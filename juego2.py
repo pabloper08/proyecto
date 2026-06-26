f95256694ca296624c101a279d8ce27c8536e2e8
pabloper08 <pabloperezoso110@gmail.com>
2026-06-26 18:38:55 -0300

    Update juego2.py

--- juego2.py
+++ juego2.py
@@ -66,3 +66,17 @@ AHORCADO_DIBUJOS = [
     =========
     """
 ]
+
+print("--- ¡BIENVENIDO AL AHORCADO! ---")
+
+# --- LEER EL ARCHIVO DE PALABRAS (FORMA BÁSICA) ---
+archivo = open("palabras.txt", "r", encoding="utf-8") #encoding="utf-8" para que lea acentos y ñ correctamente
+palabras = []
+
+for linea in archivo:
+    palabra_limpia = linea.strip()
+    if palabra_limpia != "":
+        palabras.append(palabra_limpia.lower())
+
+archivo.close()
+# --------------------------------------------------
